cd2a30daa8feb32b923cbe8e1468978395ba38f0
Akeem Lawrence <akeemwlawrence@gmail.com>
2022-11-27 15:38:43 -0600

    commit tree bc316973f6b730781571728eee7e33c81b16a464

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2; 
+
+let random = Math.round( (Math.random()) );
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+let num5 = 1;
+let num6 = 2;
+let num7 = 20;
+let max = Math.max(num5, num6, num7);
